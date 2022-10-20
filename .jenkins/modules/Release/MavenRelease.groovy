2caa50070c4a0ad7075986b470c96a6afb1cef1f
Cuba Stanley <cuba.stanley@payara.fish>
2022-10-20 16:46:55 +0100

    Override release for maven deploy

--- .jenkins/modules/Release/MavenRelease.groovy
+++ .jenkins/modules/Release/MavenRelease.groovy
@@ -1,1 +1,6 @@
-echo "No release/deployment to take place here!!"
+withMaven(jdk: CFG.jdk) {
+    sh """
+        mvn clean deploy -DaltDeploymentRepository=payara-artifacts::https://nexus.payara.fish/repository/payara-artifacts/
+        -DskipTests -Dmaven.test.skip=true
+        -Dtests.excluded=true"""
+}
