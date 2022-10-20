b46277dd185e28b2da79bf8752c9eade9cfa4bbf
Cuba Stanley <cuba.stanley@payara.fish>
2022-10-20 15:28:25 +0100

    Remove deploy override

--- .jenkins/modules/Deploy/MavenDeploy.groovy
+++
@@ -1,4 +0,0 @@
-withMaven(jdk: CFG.jdk) {
-    sh """mvn deploy -B -V  -DaltDeploymentRepository=payara-artifacts::https://nexus.payara.fish/repository/payara-artifacts/
-            -DskipTests -Dmaven.test.skip=true -Dtests.excluded=true"""
-}
