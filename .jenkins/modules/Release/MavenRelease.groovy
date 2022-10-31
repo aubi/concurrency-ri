5ae5ea83a0824f95c3e3b40ab700816255b1b0a1
Cuba Stanley <cuba.stanley@payara.fish>
2022-10-31 15:53:46 +0000

    Don't skip tests

--- .jenkins/modules/Release/MavenRelease.groovy
+++ .jenkins/modules/Release/MavenRelease.groovy
@@ -1,6 +1,4 @@
 withMaven(jdk: CFG.jdk) {
     sh """
-        mvn clean deploy -DaltDeploymentRepository=payara-artifacts::https://nexus.payara.fish/repository/payara-artifacts/
-        -DskipTests -Dmaven.test.skip=true
-        -Dtests.excluded=true"""
+        mvn clean deploy -DaltDeploymentRepository=payara-artifacts::https://nexus.payara.fish/repository/payara-artifacts/"""
 }
